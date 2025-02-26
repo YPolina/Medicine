3546155523eb082f2a59aea184a241009f98d03f
YPolina <yatsko_polina1@mail.ru>
2025-02-26 13:36:36 +0100

    Changes: - Separate Datasets for each type of model - Separate file for training models

--- BELKA/functionality/data_preparation.py
+++ BELKA/functionality/data_preparation.py
@@ -8,10 +8,12 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from rdkit.Chem import rdFingerprintGenerator, AllChem
 from rdkit.DataStructs import ConvertToNumpyArray
 from rdkit import Chem
+from transformers import AutoTokenizer, AutoModel
 import numpy as np
 import tqdm
 import os 
 import pickle
+from pytorch_lightning.loggers import CSVLogger
 
 
 import torch
@@ -28,35 +30,177 @@ class CustomDataset(Dataset):
         """
         self.smiles = data['molecule_smiles'].values
         self.labels = data['binds'].values
+        self.features = None
 
     def __len__(self):
         return len(self.smiles)
+    
 
     def __getitem__(self, idx):
-        smile = self.smiles[idx]  
-        label = torch.tensor(self.labels[idx], dtype=torch.float16) 
-        return smile, label
+        pass
+    
+class CNN_Dataset(CustomDataset):
+    def __init__(self, data, max_length=142):
+        """
+        Dataset that generates additional features for SMILES strings
 
-def prepare_dataloaders(data, batch_size=100000):
-    """
-    Splits data into train/val sets and returns DataLoaders
+        Args:
+            data (pd.DataFrame): Data with 'molecule_smiles' and 'binds' columns
+            max_length (int): Maximum length for the SMILES encoding
 
-    Args:
-        data (pd.DataFrame): Data containing 'molecule_smiles' and 'binds'
-        batch_size (int): Batch size
+        """
+        super().__init__(data)
+        self.max_length = max_length
 
-    Returns:
-        train_loader, val_loader: PyTorch DataLoaders for training and validation
-    """
-    train_data, val_data = train_test_split(data, test_size=0.1, random_state=42)
+    def create_features(self, smiles_batch):
+        """
+        Encodes SMILES strings into fixed-length numerical representations using CNN features.
 
-    train_dataset = CustomDataset(train_data)
-    val_dataset = CustomDataset(val_data)
+        Args:
+            smiles_batch (list of str): List of SMILES strings.
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+        Returns:
+            Tensor: Encoded SMILES representations.
+        """
+        enc_dict = {
+            'l': 1, 'y': 2, '@': 3, '3': 4, 'H': 5, 'S': 6, 'F': 7, 'C': 8, 'r': 9, 's': 10, '/': 11, 'c': 12, 'o': 13,
+            '+': 14, 'I': 15, '5': 16, '(': 17, '2': 18, ')': 19, '9': 20, 'i': 21, '#': 22, '6': 23, '8': 24, '4': 25, '=': 26,
+            '1': 27, 'O': 28, '[': 29, 'D': 30, 'B': 31, ']': 32, 'N': 33, '7': 34, 'n': 35, '-': 36
+        }
 
-    return train_loader, val_loader
+        def encode_smile(smile):
+            """Encodes a SMILES string to numerical values and pads it."""
+            encoded = [enc_dict.get(char, 0) for char in smile]
+            padded = encoded + [0] * (self.max_length - len(encoded))  # Padding to max_length
+            return np.array(padded[:self.max_length], dtype=np.uint8)
+
+        # Apply encoding to all SMILES in the batch
+        smiles_enc = np.stack([encode_smile(smile) for smile in smiles_batch])
+        return torch.tensor(smiles_enc, dtype=torch.float32)
+    
+    def __getitem__(self, idx):
+        features = torch.tensor(self.create_features(self.smiles[idx]), dtype = torch.float32)
+        label = torch.tensor(self.labels[idx], dtype=torch.long)
+        return features, label
+    
+
+class Emb_Dataset(CustomDataset):
+    def __init__(self, data, model_name):
+        """
+        Dataset that generates additional features for SMILES strings
+
+        Args:
+            data (pd.DataFrame): Data with 'molecule_smiles' and 'binds' columns
+            model_name (str): The pre-trained model name
+
+        """
+        super().__init__(data)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModel.from_pretrained(model_name)
+        
+        self.model.eval()
+
+    def create_features(self, smiles_batch):
+        """
+        Encodes SMILES strings into fixed-length numerical representations using CNN features.
+
+        Args:
+            smiles_batch (list of str): List of SMILES strings.
+
+        Returns:
+            Tensor: Tokenize SMILES representations.
+        """
+        tokens = self.tokenizer(smiles_batch, padding=True, truncation=True, max_length=512, return_tensors='pt')
+
+        with torch.no_grad():
+            outputs = self.model(**tokens)
+    
+        cls_embeddings = outputs.last_hidden_state[:, 0, :]  
+        
+        return cls_embeddings
+
+    def __getitem__(self, idx):
+       
+        # Create embeddings for the SMILES string
+        #from [1, embedding_size] to [embedding_size])
+        features = self.create_features([self.smiles[idx]]).squeeze(0)
+        label = torch.tensor(self.labels[idx], dtype=torch.long)
+        return features, label
+    
+class GBT_Dataset(CustomDataset):
+    def __init__(self, data, ):
+        
+        super().__init__(data)
+
+        self.fpg = rdFingerprintGenerator.GetMorganGenerator(radius=2, fpSize=1024, includeChirality=True)
+
+    def create_features(self, smiles_batch):
+
+        """
+        Compute fingerprints for a batch of SMILES
+        """
+        results = []
+        for smiles in smiles_batch:
+            mol = Chem.MolFromSmiles(smiles.replace('[Dy]', '[H]'))
+            if mol is None:
+                results.append((np.zeros(1024, dtype=np.int8), {}))
+            else:
+                fp = fpg.GetCountFingerprint(mol)
+                arr = np.zeros(1024, dtype=np.int8)
+                ConvertToNumpyArray(fp, arr)
+                results.append(arr)
+        return np.array(results)
+    
+    def __getitem__(self, idx):
+       
+        features = self.create_features([self.smiles[idx]]) 
+        features = torch.tensor(features[0], dtype=torch.float32)  
+        
+        label = torch.tensor(self.labels[idx], dtype=torch.long)
+        return features, label
+
+class Llama_Dataset(CustomDataset):
+    def __init__(self, data, model_name):
+        """
+        Dataset that generates additional features for SMILES strings
+
+        Args:
+            data (pd.DataFrame): Data with 'molecule_smiles' and 'binds' columns
+            model_name (str): The pre-trained model name
+
+        """
+        super().__init__(data)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModel.from_pretrained(model_name)
+        
+        self.model.eval()
+
+    def create_features(self, smiles_batch):
+        """
+        Encodes SMILES strings into fixed-length numerical representations using CNN features.
+
+        Args:
+            smiles_batch (list of str): List of SMILES strings.
+
+        Returns:
+            Tensor: Tokenize SMILES representations.
+        """
+        tokens = self.tokenizer(smiles_batch, padding=True, truncation=True, max_length=512, return_tensors='pt')
+        tokens = {key: val for key, val in tokens.items()}
+
+        outputs = self.model(input_ids=tokens["input_ids"])
+        pooled_output = outputs.last_hidden_state.mean(dim=1)
+
+        return pooled_output
+
+    def __getitem__(self, idx):
+       
+        # Create embeddings for the SMILES string
+        #from [1, embedding_size] to [embedding_size])
+        features = self.create_features([self.smiles[idx]]).squeeze(0)
+        label = torch.tensor(self.labels[idx], dtype=torch.long)
+        return features, label
+
 
 
 
@@ -76,11 +220,13 @@ def train_model(model_name, protein_name, model, train_loader, val_loader, epoch
         trainer: The trainer object after training
     """
     
+    logger = CSVLogger("logs", name=model_name)
+
     # Define checkpointing: Save best model based on validation loss
     checkpoint_callback = ModelCheckpoint(
         dirpath="./checkpoints",  
         filename=f"{protein_name}_{model_name}-{{epoch}}-{{val_loss:.4f}}", 
-        monitor="val_loss", 
+        monitor="val_loss",
         save_top_k=3,
         mode="min",
         save_last=True,
@@ -89,33 +235,16 @@ def train_model(model_name, protein_name, model, train_loader, val_loader, epoch
 
     trainer = pl.Trainer(
         max_epochs=epochs,
-        accelerator="gpu" if torch.cuda.is_available() else "cpu",
+        accelerator="auto",
+        devices=1,
         log_every_n_steps=2,
-        callbacks=[checkpoint_callback]
+        callbacks=[checkpoint_callback],
+        logger = logger
     )
-
     trainer.fit(model, train_loader, val_loader)
 
     return trainer
 
-fpg = rdFingerprintGenerator.GetMorganGenerator(radius=2, fpSize=1024, includeChirality=True)
-
-def compute_fps(smiles_batch):
-
-    """
-    Compute fingerprints for a batch of SMILES
-    """
-    results = []
-    for smiles in smiles_batch:
-        mol = Chem.MolFromSmiles(smiles.replace('[Dy]', '[H]'))
-        if mol is None:
-            results.append((np.zeros(1024, dtype=np.int8), {}))
-        else:
-            fp = fpg.GetCountFingerprint(mol)
-            arr = np.zeros(1024, dtype=np.int8)
-            ConvertToNumpyArray(fp, arr)
-            results.append(arr)
-    return np.array(results)
 
 def train_gbt(protein_name, train_loader, lgb_params=None, save_dir="./checkpoints"):
     """
@@ -133,17 +262,14 @@ def train_gbt(protein_name, train_loader, lgb_params=None, save_dir="./checkpoin
 
     lgb_cls = LGBMClassifier(**lgb_params)
 
-    # Iterate over the training data batches
-    for smiles_batch, label_batch in tqdm(train_loader, desc=f"Training {protein_name} (Training)"):
-        smiles_batch = [item['molecule_smiles'] for item in smiles_batch]
-        labels_batch = [item['binds'] for item in label_batch]
+    for features_batch, labels_batch in tqdm(train_loader, desc=f"Training {protein_name} (Training)"):
         
-        X_batch = compute_fps(smiles_batch) 
-        y_batch = np.array(labels_batch)
+        X_batch = features_batch.numpy() 
+        y_batch = labels_batch.numpy()
         
-        # Fit the model with the current batch and the full validation set
         lgb_cls.fit(X_batch, y_batch, eval_metric='auc', init_model=lgb_cls.booster_ if hasattr(lgb_cls, "booster_") else None)
-
+    
+    lgb_cls.save_model(f"{save_dir}/{protein_name}_lgb_model.txt")
 
     return lgb_cls
 
--- BELKA/functionality/models.py
+++ BELKA/functionality/models.py
@@ -18,23 +18,23 @@ class BaseBinaryClassifierLightning(pl.LightningModule):
         self.save_hyperparameters()
         self.learning_rate = learning_rate
         self.auroc = BinaryAUROC()
-        self.loss_fn = nn.BCEWithLogitsLoss()
+        self.loss_fn = nn.CrossEntropyLoss()
         self.validation_step_outputs = []
     
     def training_step(self, batch, batch_idx):
-
         smiles_batch, labels = batch 
         logits = self(smiles_batch)
         loss = self.loss_fn(logits.squeeze(), labels)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        
         smiles_batch, labels = batch 
-        
         logits = self(smiles_batch)
         loss = self.loss_fn(logits.squeeze(), labels)
         auroc = self.auroc(logits.squeeze(), labels)
+
+        self.log("val_loss", loss, prog_bar=True, logger=True, on_epoch=True)
+        self.log("val_auroc", auroc, prog_bar=True, logger=True, on_epoch=True)
 
         self.validation_step_outputs.append({"val_loss": loss, "val_auroc": auroc})
         return {"val_loss": loss, "val_auroc": auroc}
@@ -45,26 +45,16 @@ class BaseBinaryClassifierLightning(pl.LightningModule):
 
         avg_loss = round(avg_loss.item(), 3)
         avg_auroc = round(avg_auroc.item(), 3)
-
-        self.log("val_loss", avg_loss, prog_bar=True, logger=True)
-        self.log("val_auroc", avg_auroc, prog_bar=True, logger=True)
+        
+        self.log("avg_val_loss", avg_loss, prog_bar=True, logger=True)
+        self.log("avg_val_auroc", avg_auroc, prog_bar=True, logger=True)
 
         self.validation_step_outputs.clear()
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-            optimizer, mode="min", factor=0.1, patience=3, verbose=True
-        )
-        return {
-            "optimizer": optimizer,
-            "lr_scheduler": {
-                "scheduler": scheduler,
-                "monitor": "val_loss",
-                "interval": "epoch",
-                "frequency": 1
-            }
-        }
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
+        return [optimizer], [scheduler]
 
     def predict(self, batch):
 
@@ -93,44 +83,18 @@ class CNNBinaryClassifierLightning(BaseBinaryClassifierLightning):
         self.fc1 = nn.Linear(64 * ((self.max_length // 2) // 2), 128)
         self.fc2 = nn.Linear(128, output_dim)
 
-    def CNN_features(self, smiles_batch):
-        """
-        Encodes SMILES strings into fixed-length numerical representations
-        
-        Args:
-            smiles_batch (list of str): Batch of SMILES strings
-
-        Returns:
-            Tensor: Encoded and padded SMILES representations
-        """
-        enc_dict = {
-            'l': 1, 'y': 2, '@': 3, '3': 4, 'H': 5, 'S': 6, 'F': 7, 'C': 8, 'r': 9, 's': 10, '/': 11, 'c': 12, 'o': 13,
-            '+': 14, 'I': 15, '5': 16, '(': 17, '2': 18, ')': 19, '9': 20, 'i': 21, '#': 22, '6': 23, '8': 24, '4': 25, '=': 26,
-            '1': 27, 'O': 28, '[': 29, 'D': 30, 'B': 31, ']': 32, 'N': 33, '7': 34, 'n': 35, '-': 36
-        }
-        
-        def encode_smile(smile):
-            encoded = [enc_dict.get(char, 0) for char in smile]  
-            padded = encoded + [0] * (self.max_length - len(encoded))
-            return np.array(padded[:self.max_length], dtype=np.uint8)
-
-        smiles_enc = np.stack([encode_smile(smile) for smile in smiles_batch])
-        
-        return torch.tensor(smiles_enc, dtype=torch.float32)
-
-    def forward(self, smiles_batch):
+    def forward(self, features):
         """
         Forward pass of CNN model
         
         Args:
-            smiles_batch (list of str): Batch of SMILES strings
+            features (tensor): encoded SMILES strings
         
         Returns:
             Tensor: Predicted probability scores
         """
-        x = self.CNN_features(smiles_batch)
         #(batch_size, seq_len)
-        x = x.unsqueeze(1)
+        x = features.unsqueeze(1)
         # add chanel dimension(batch_size, 1, seq_len)
         x = self.pool(self.leakyrelu(self.conv1(x)))
         x = self.pool(self.leakyrelu(self.conv2(x)))
@@ -139,9 +103,8 @@ class CNNBinaryClassifierLightning(BaseBinaryClassifierLightning):
         #flatten
         x = self.dropout(self.leakyrelu(self.fc1(x)))
         logits = self.fc2(x)
-        output = self.sigmoid(logits)
-
-        return output
+        
+        return logits
 
 class ChemBertaBinaryClassifierLightning(BaseBinaryClassifierLightning):
     def __init__(self, model_name="seyonec/PubChem10M_SMILES_BPE_450k",
@@ -151,43 +114,27 @@ class ChemBertaBinaryClassifierLightning(BaseBinaryClassifierLightning):
         super().__init__(learning_rate)
 
         self.config = AutoConfig.from_pretrained(model_name)
-        self.model = AutoModel.from_pretrained(model_name)
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-        for param in self.model.parameters():
-            param.requires_grad = False
-
-        #unfreeze last layer for fine-tuning
-        for param in self.model.encoder.layer[-1].parameters():  
-            param.requires_grad = True
 
         self.dropout = nn.Dropout(dropout_rate)
         self.fc1 = nn.Linear(self.config.hidden_size, 128)
         self.fc2 = nn.Linear(128, num_labels)
 
 
-    def forward(self, smiles_batch):
+    def forward(self, features):
         """
         Forward pass through ChemBERTa model
 
         Args:
-            smiles_batch (list of str): Batch of SMILES strings
+            features (Tensor): Preprocessed feature tensor, shape (batch_size, feature_size)
 
         Returns:
             Tensor: Logits for binary classification
         """
-        tokens = self.tokenizer(smiles_batch, padding=True, truncation=True, max_length=512, return_tensors='pt')
-        tokens = {key: val for key, val in tokens.items()}
-
-        outputs = self.model(**tokens)
-        cls_embedding = outputs.last_hidden_state[:, 0]
-
-        x = self.fc1(cls_embedding)
-        x = self.dropout(x)
+        
+        x = self.dropout(self.fc1(features))
         logits = self.fc2(x)
 
         return logits
-
 
 class MolLlamaBinaryClassifierLightning(BaseBinaryClassifierLightning):
     def __init__(self, llama_model="meta-llama/Llama-2-7b-chat-hf", 
@@ -196,50 +143,25 @@ class MolLlamaBinaryClassifierLightning(BaseBinaryClassifierLightning):
                  use_lora=True, learning_rate=2e-5):
         super().__init__(learning_rate)
 
-        quantization_config = BitsAndBytesConfig(
-            load_in_4bit=True
-        )
-
-        self.llama = AutoModelForCausalLM.from_pretrained(
-            llama_model,
-            quantization_config=quantization_config,
-            device_map="auto"
-        )
-
-
-        if use_lora:
-            config = LoraConfig(
-                task_type=TaskType.SEQ_CLS,
-                inference_mode=False,
-                r=16, lora_alpha=32, lora_dropout=0.1,
-                target_modules=["query", "key", "value", "dense"]
-            )
-            self.llama = get_peft_model(self.llama, config)
-
-        self.tokenizer = LlamaTokenizer.from_pretrained(llama_model)
+        self.config = AutoConfig.from_pretrained(llama_model)
 
         encoder_layer = nn.TransformerEncoderLayer(d_model=embedding_dim, nhead=num_heads)
         self.self_attention = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
-        self.projection = nn.Linear(embedding_dim, self.llama.config.hidden_size)
-        self.classifier = nn.Linear(self.llama.config.hidden_size, num_classes)
+        self.projection = nn.Linear(embedding_dim, self.config.hidden_size)
+        self.classifier = nn.Linear(self.config.hidden_size, num_classes)
 
-    def forward(self, smiles_batch):
+    def forward(self, features):
         """
-        Forward pass through MolLLaMA model
+        Forward pass through MolLama model
 
         Args:
-            smiles_batch (list of str): Batch of SMILES strings
+            features (Tensor): Preprocessed feature tensor, shape (batch_size, feature_size)
 
         Returns:
             Tensor: Logits for binary classification
         """
-        tokens = self.tokenizer(smiles_batch, padding=True, truncation=True, max_length=512, return_tensors='pt')
-        tokens = {key: val for key, val in tokens.items()}
 
-        outputs = self.model(input_ids=tokens["input_ids"])
-        pooled_output = outputs.last_hidden_state.mean(dim=1)
-
-        x = self.fc1(pooled_output)
+        x = self.fc1(features)
         x = self.dropout(x)
         logits = self.fc2(x)
 
@@ -249,39 +171,30 @@ class MolLlamaBinaryClassifierLightning(BaseBinaryClassifierLightning):
 class MolFormerXLBinaryClassifierLightning(BaseBinaryClassifierLightning):
     def __init__(self, model_name="ibm/MoLFormer-XL-both-10pct", 
                  hidden_dim=256, output_dim=1, 
-                 dropout_rate=0.1, learning_rate=2e-5, use_lora = True):
+                 dropout_rate=0.1, learning_rate=2e-5):
         super().__init__(learning_rate)
 
-        self.molformer = AutoModel.from_pretrained(model_name, trust_remote_code=True)
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-
-        if use_lora:
-            lora_config = LoraConfig(
-                task_type=TaskType.SEQ_CLS,
-                inference_mode=False, 
-                r=16,
-                lora_alpha=32, 
-                lora_dropout=0.1,
-                target_modules=["query", "key", "value", "dense"]
-            )
-            self.molformer = get_peft_model(self.molformer, lora_config)
+        self.config = AutoConfig.from_pretrained(model_name)
 
         self.classifier = nn.Sequential(
-            nn.Linear(self.molformer.config.hidden_size, hidden_dim),
+            nn.Linear(self.config.hidden_size, hidden_dim),
             nn.ReLU(),
             nn.Dropout(dropout_rate),
             nn.Linear(hidden_dim, output_dim)
         )
 
-    def forward(self, smiles_batch):
-        tokens = self.tokenizer(
-            smiles_batch, padding=True, truncation=True, max_length=512, return_tensors="pt"
-        )
-        tokens = {key: val for key, val in tokens.items()}
+    def forward(self, features):
+        """
+        Forward pass through MolFormer model
 
-        outputs = self.molformer(**tokens)
-        cls_embedding = outputs.last_hidden_state[:, 0]
+        Args:
+            features (Tensor): Preprocessed feature tensor, shape (batch_size, feature_size)
 
-        logits = self.classifier(cls_embedding).squeeze(-1)
+        Returns:
+            Tensor: Logits for binary classification
+        """
+        x = self.dropout(self.fc1(features))
+        logits = self.fc2(x)
+
         return logits
     
